ead477a90aefa4e7f20c013e4f4b48478603eff9
DevAnsar <ansarmirzayi@gmail.com>
2023-02-11 15:26:35 +0330

    prettier cahnges

--- tests/cartProvider.test.tsx
+++ tests/cartProvider.test.tsx
@@ -2,44 +2,37 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom'
 
-import { CartProvider, CartContext } from '../src';
+import { CartProvider, CartContext } from '../src'
 
 describe('CartProvider render', () => {
-    it('renders without crashing', () => {
-        render(
-            <CartProvider>
-                <div>test</div>
-            </CartProvider>,
-        )
-    })
+  it('renders without crashing', () => {
+    render(
+      <CartProvider>
+        <div>test</div>
+      </CartProvider>,
+    )
+  })
 })
 
 const customRender = (ui: React.ReactNode, providerProps: any, renderOptions: any) => {
-    return render(
-        <CartContext.Provider value={...providerProps}>{ui}</CartContext.Provider>,
-        renderOptions,
-    )
+  return render(<CartContext.Provider value={...providerProps}>{ui}</CartContext.Provider>, renderOptions)
 }
 
-
-
 test('CartContext composes full name from first, last', () => {
-    const providerProps = {
-
-        cart: { name: "ansar" },
-        addToCart: () => console.log('this is a test'),
-
-    }
-    customRender(
-        <CartContext.Consumer>
-            {value => (
-                <div>
-                    <span children={`name: ${value.cart.name}`} />
-                </div>
-            )}
-        </CartContext.Consumer>,
-        providerProps,
-        {}
-    )
-    expect(screen.getByText(/^name:/).textContent).toBe('name: ansar')
+  const providerProps = {
+    cart: { name: 'ansar' },
+    addToCart: () => console.log('this is a test'),
+  }
+  customRender(
+    <CartContext.Consumer>
+      {(value) => (
+        <div>
+          <span children={`name: ${value.cart.name}`} />
+        </div>
+      )}
+    </CartContext.Consumer>,
+    providerProps,
+    {},
+  )
+  expect(screen.getByText(/^name:/).textContent).toBe('name: ansar')
 })
